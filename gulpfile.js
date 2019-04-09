5706f43a77a57bc06f1cca1b5b2476647cbde783
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-04-09 20:10:18 +0200

    change

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,7 @@
-const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const less = require('gulp-less');
 const { src, dest, parallel, task } = require('gulp');
 const fileroot = "./";
-task ('js', () => {
-  return src(fileroot + 'index.js')
-    .pipe(rename('index.min.js'))
-	.pipe(uglify())
-	.pipe(dest(fileroot));
-});
-task ('jsr', () => {
-  return src(fileroot + 'manager/main_process/index.js')
-    .pipe(rename('index.min.js'))
-	.pipe(uglify())
-	.pipe(dest(fileroot + 'manager/main_process'));
-});
-task ('jsrd', () => {
-  return src(fileroot + 'manager/main_process/data.js')
-    .pipe(rename('data.min.js'))
-	.pipe(uglify())
-	.pipe(dest(fileroot + 'manager/main_process'));
-});
 task ('css', () => {
   return src(fileroot + 'content/sheet1.less')
 	.pipe(rename('csscompile.less'))
