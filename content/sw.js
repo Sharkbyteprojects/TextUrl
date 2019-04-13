4af2ee64563543e522bbaaa324a06abc2109f6ef
Sharkbyteprojects <sharkbyteprojects@protonmail.com>
2019-04-13 07:14:50 +0200

    modified:   content/sw.js

--- content/sw.js
+++ content/sw.js
@@ -1,13 +1,26 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(
-        [
+var CACHE_NAME = 'sharkpage'
+var cads = [
           '/sheet1.less',
           '/ico.png',
           '/'
-        ]
-      );
+];
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME);
+    .then(function(cache){
+      console.log("MAKING WEBPAGE FASTER");
+      return cache.addAll(cads);
     })
   );
+});
+self.addEventListener('fetch', function(event){
+	event.respondWith(
+	   caches.match(event.request)
+	   .then(function(response){
+		   if(response){
+			   return response;
+		   }
+		   return fetch(event.request),
+	   });
+	);
 });
